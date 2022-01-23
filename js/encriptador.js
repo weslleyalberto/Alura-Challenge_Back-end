b7b2ef75c92cc542b21ff654461ed0802c67d81f
Weslley Alberto <weslleyalberto@gmail.com>
2022-01-23 14:15:54 -0300

    Mudança da função codificador usando switch

--- js/encriptador.js
+++ js/encriptador.js
@@ -1,32 +1,31 @@
 var msg = document.querySelector("#msg");
 var inputTexto = document.querySelector("#input-texto");
 
-function codificador(palavra){
-    var novapalavra = [] ;
-    
-    for(var i =0; i < palavra.length;i++){
-     if(palavra[i] == "e"){
-         novapalavra[i] = "enter";
-       }
-       else if (palavra[i] == "i"){
-         novapalavra[i] = "imes";
-       }
-       else if(palavra[i] == "a"){
-         novapalavra[i] = "ai";
-       }
-       else if(palavra[i] == "o"){
-         novapalavra[i] = "ober";
-       }
-       else if(palavra[i] == "u"){
-         novapalavra[i] = "ufat";
-       }
-      
-       else{
-         novapalavra[i] = palavra[i];
-       }
-      
-    }
-    return novapalavra.join("");
+function codificador(texto){
+    var novoNome = [] ;
+    for(var i =0; i < texto.length;i++){
+      switch(texto[i]){
+         case 'a':
+             novoNome[i] = "ai";
+             break;
+             case 'u':
+                 novoNome[i] = "ufat";
+                 break;
+                 case 'o':
+                     novoNome[i] = "ober";
+                     break;
+                     case 'e':
+                         novoNome[i]  = "enter";
+                         break;
+                         case 'i':
+                             novoNome[i] = "imes";
+                             break;
+                             default:
+                                 novoNome[i] = texto[i];
+  
+      }
+  }
+    return novoNome.join("");
   }
   /*function validaCampos(inputText){
 
