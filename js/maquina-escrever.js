4b859f9dca275269b630bdb2d16ee383d8982392
Weslley Alberto <weslleyalberto@gmail.com>
2022-01-27 01:28:00 -0300

    Renomeada pasta images para assets e seus arquivos com a descrição correta de cada um

--- js/maquina-escrever.js
+++ js/maquina-escrever.js
@@ -1,7 +1,7 @@
 
                         function typeWrite(elemento) {
-                            const textArray = elemento.innerHTML.split('');
-                            elemento.innerHTML = ' ';
+                            const textArray = elemento.innerHTML.split(''); // Transforma Array
+                            elemento.innerHTML = ' '; // Limpa Elemento
                             textArray.forEach(function (letra, i) {
                                 setTimeout(function () {
                                     elemento.innerHTML += letra;
@@ -11,10 +11,16 @@
                         const titulo = document.querySelector('#label-text');
                         const titulo2 = document.querySelector('#title-text');
                         var footdata = document.querySelector("#footer-data");
-                      
+                        
+                        
                         typeWrite(titulo);
                         typeWrite(titulo2);
                         typeWrite(footdata);
+                       /* var algo = ["Marina","Jonas","Gabriel"];
+                        algo.forEach(function(nomes,i){
+                            console.log(nomes);
+                            console.log(i);
+                        }, );*/
                       
                      
                   
